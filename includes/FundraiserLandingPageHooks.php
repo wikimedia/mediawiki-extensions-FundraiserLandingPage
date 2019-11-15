227f2378637d5b40f39b2a503855bf4cc4a4d090
Max Semenik <maxsem.wiki@gmail.com>
2019-11-15 00:10:40 -0800

    Stop passing objects by reference
    Bug: T193950
    Change-Id: I26b7a2551dfb0652afe197ab4be392a33be67222

--- includes/FundraiserLandingPageHooks.php
+++ includes/FundraiserLandingPageHooks.php
@@ -4,11 +4,11 @@ class FundraiserLandingPageHooks {
 	 * Register the parser function hooks 'switchlanguage' and 'switchcountry'
 	 * with the MW backend.
 	 *
-	 * @param Parser &$parser The MW parser object to hook into.
+	 * @param Parser $parser The MW parser object to hook into.
 	 *
 	 * @return bool Always true
 	 */
-	public static function onParserFirstCallInit( &$parser ) {
+	public static function onParserFirstCallInit( $parser ) {
 		$parser->setFunctionHook(
 			'switchlanguage',
 			'FundraiserLandingPage::fundraiserLandingPageSwitchLanguage'
